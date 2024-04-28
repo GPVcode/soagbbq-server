2f61fefad38077cc2a8794e1b2bbf6870f2658ae
GPV <gvill005@gmail.com>
2024-04-27 20:17:13 -0700

    server can now run

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { google } from 'googleapis';
-
+import dotenv from 'dotenv'
 const app = express();
+dotenv.config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
