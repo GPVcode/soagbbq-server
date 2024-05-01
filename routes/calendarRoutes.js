a3b8656e98ec745054104336dc707b95dbdfe97f
GPV <gvill005@gmail.com>
2024-04-30 21:39:00 -0700

    clean up

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -7,7 +7,6 @@ import { listEvents } from '../services/googleCalendarService.js';
 router.get('/events', async (req, res) => {
     try{
         const events = await listEvents();
-        console.log("EVENTS: ", events)
         res.json(events);
     } catch(error){
         console.error('Error retrieving events:', error);
