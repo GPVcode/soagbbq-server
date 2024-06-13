14ef159b246e66d7a72953545f28ac6ea2a816c1
GPV <gvill005@gmail.com>
2024-06-13 12:13:59 -0700

    updated cache-duration to 30 secconds

--- services/googleCalendarService.js
+++ services/googleCalendarService.js
@@ -14,7 +14,7 @@ const calendar = google.calendar({ version: 'v3', auth: serviceAuth });
 
 let cachedEvents = null;
 let cacheTimestamp = 0;
-const CACHE_DURATION = 86400000;  // 1 day in milliseconds
+const CACHE_DURATION = 30000;  // 30 day in milliseconds
 
 const formatEventDates = (event) => {
     const formattedEvent = { ...event };
